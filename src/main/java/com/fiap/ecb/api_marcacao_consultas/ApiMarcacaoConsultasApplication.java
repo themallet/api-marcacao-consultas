7b0b16f7a6c382f6da514a4df96479ce4e74e39f
themallet <amandamaiaballet@gmail.com>
2025-03-10 08:38:25 -0300

    Marcação de Consultas

---
+++ src/main/java/com/fiap/ecb/api_marcacao_consultas/ApiMarcacaoConsultasApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ecb.api_marcacao_consultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMarcacaoConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMarcacaoConsultasApplication.class, args);
+	}
+
+}
